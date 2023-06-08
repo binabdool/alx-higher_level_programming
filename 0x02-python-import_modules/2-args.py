f12c6cffd10446709dcb5cabf3a004c232f2d945
binabdool <seaneverdry877@gmail.com>
2023-06-08 15:19:23 -0700

    import_module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     import sys
-
-    args = sys.argv[1:]
-    num_args = len(args)
-
-    if num_args == 0:
-        print("0 arguments")
-        print(".")
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(1))
     else:
-        print("{} argument{}:".format(num_args, "" if num_args == 1 else "s"))
-        for num, argz in enumerate(args):
-            print("{}: {}".format(num + 1, argz))
+        print("{} arguments:".format(len(sys.argv) - 1))
+    if len(sys.argv) > 1:
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
